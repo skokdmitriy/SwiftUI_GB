6c35f21618fdaff656493c165f3138810ebd39f5
skokdmitriy <skokdmitriy@gmail.com>
2022-07-18 22:42:04 +0700

    Initial Commit

---
+++ SwiftUI_GB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_GB
+//
+//  Created by Дмитрий Скок on 18.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_GB/SwiftUI_GBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GBApp.swift
+//  SwiftUI_GB
+//
+//  Created by Дмитрий Скок on 18.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
